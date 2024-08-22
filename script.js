85390d6be8fc17c50e6464776c801e9561d1b69d
davidgomezcode <davidgomezcode@gmail.com>
2024-08-22 18:15:42 -0500

    Improvement of the burger menu and start of the relation of the pins and the activities

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 //Selectors:
 const menuBurgerIMG = document.querySelector(".menuBurgerIMG");
 const menuBurgerSpace = document.querySelector("#menuBurgerSpace");
-const sendButtonParent = document.querySelector(".sendButtonParent");
+const sendButton = document.querySelector(".sendButton");
+const menuBurgerSpaceDivActivities = document.querySelector(
+  ".menuBurgerSpaceDivActivities"
+);
+
+const buttonUp = document.querySelector(".buttonUp");
+const buttonDown = document.querySelector(".buttonDown");
 
 // Global variables
 let coords;
@@ -36,25 +42,44 @@ let mapReal = L.tileLayer(
 //Functions
 //Function - insertion of new activity:
 // variable that contains the structure of the new activity - ON CONSTRUCTION.
-const html = `<article class="activity activity-1">
-                <div class="inputParent">
-                  <input class="input activityInput" placeholder="Activity" />
-                </div>
-                <div class="inputParent">
-                  <input class="input timeInput" placeholder="Time" />
-                </div>
 
-                <div class="inputParent">
-                  <textarea
-                    name=""
-                    id=""
-                    class="input observationsInput"
-                    placeholder="Observations"
-                    rows="3"
-                  >
-                  </textarea>
-                </div>
-              </article>`;
+const createActivity = function () {
+  let lastActivityNumber =
+    menuBurgerSpaceDivActivities.lastElementChild.dataset.number;
+
+  const html = `<article data-number="${
+    Number(lastActivityNumber) + 1
+  }" class="activity">
+    <div class="inputParent">
+      <input class="input activityInput" placeholder="Activity" />
+    </div>
+    <div class="inputParent">
+      <input class="input timeInput" placeholder="Time" />
+    </div>
+
+    <div class="inputParent">
+      <textarea
+        name=""
+        id=""
+        class="input observationsInput"
+        placeholder="Observations"
+        rows="3"
+      >
+      </textarea>
+    </div>
+  </article>`;
+  menuBurgerSpaceDivActivities.lastElementChild.insertAdjacentHTML(
+    "afterend",
+    html
+  );
+};
+
+const openBurgerMenu = function () {
+  menuBurgerSpace.classList.toggle("hidden");
+  buttonUp.classList.toggle("hidden");
+  buttonDown.classList.toggle("hidden");
+  sendButton.classList.toggle("hidden");
+};
 
 //Function of map and geolocation of user:
 navigator.geolocation.getCurrentPosition(function (position) {
@@ -91,69 +116,59 @@ navigator.geolocation.getCurrentPosition(function (position) {
 
   //Event listener to difFerentiate between the clicks over tu whereAmIButtonIMG and over the rest of the map:
   document.addEventListener("click", function (e) {
+    // console.log(e.target);
+    // console.log(!menuBurgerSpace.classList.contains("hidden"));
     if (e.target.classList.contains("whereAmIButtonIMG")) {
+      // if the user clicks the where am i button, then we create a marker with the user coords and locate the viewport on that mark:
       L.marker(coords).addTo(map);
       map.locate({ setView: true });
     } else if (e.target.classList.contains("sendButton")) {
-      sendButtonParent.insertAdjacentHTML("beforebegin", html);
+      // if the user clicks the sendbutton we crate a new activity -THIS SHOULD BE REPLACED WITH SAVING THE INFO OF ALL THE ACTIVITIES.
     } else if (
-      //if the click is not on
+      //if the click is not on the following elements:
       // the where am I button
       !e.target.classList.contains("whereAmIButtonIMG") && //the burger menu
       !e.target.classList.contains("menuBurgerIMG") && //the input of activities
       !e.target.classList.contains("activityInput") && // the input
-      !e.target.classList.contains("input") && // the activity
-      !e.target.classList.contains("activity") && //the burger menu space
+      !e.target.classList.contains("input") && // tHE BUTTONS SEND AND UP AND DOWN:
       !e.target.classList.contains("sendButton") &&
-      !e.target.classList.contains("menuBurgerSpaceDiv") && // It is not the send p¿button
+      !e.target.classList.contains("buttonUp") &&
+      !e.target.classList.contains("buttonDown") && // the activity
+      !e.target.classList.contains("activity") && //the burger menu space
+      !e.target.classList.contains("menuBurgerSpaceDiv") &&
+      !e.target.classList.contains("menuBurgerSpaceDivActivities") && // It is not the send p¿button
       e.target.id !== "menuBurgerSpace"
     ) {
-      // Then, create a marker where the user clicked.
-      let { lat, lng } = mapEvent.latlng;
-      L.marker([lat, lng]).addTo(map);
+      //tHEN IT
+      if (!menuBurgerSpace.classList.contains("hidden")) {
+        openBurgerMenu();
+      } else if (menuBurgerSpace.classList.contains("hidden")) {
+        // Then, create a marker where the user clicked.
+        let { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map);
+        createActivity();
+        openBurgerMenu();
+      }
     }
-    // else if (e.target.classList.contains("menuBurgerIMG")) {
-    //   menuBurgerSpace.classList.toggle("hidden"); // we toggle the space where the activities are going to be listed
-
-    //   console.log(!menuBurgerSpace.classList.contains("hidden"));
-    //   //We listens if the user is in or out the menu burger
-    //   if (!menuBurgerSpace.classList.contains("hidden")) {
-    //     console.log(e.target);
-
-    //     menuBurgerSpace.addEventListener("click", function () {
-    //       console.log("We are clicking the burger menu");
-    //     });
-
-    //     // menuBurgerSpace.addEventListener("mouseenter", function () {
-    //     //   console.log("the mouse entered in the burger menu");
-    //     // });
-    //     // menuBurgerSpace.addEventListener("mouseleave", function () {
-    //     //   console.log("the mouse left in the burger menu");
-    //     // });
-    //   }
-    // }
   });
 
   menuBurgerIMG.addEventListener("click", function () {
-    menuBurgerSpace.classList.toggle("hidden");
-    // menuBurgerSpace.addEventListener("mouseenter", function () {
-    //   console.log("the mouse entered in the burger menu");
-    // });
-
-    // if (!menuBurgerSpace.classList.contains("hidden")) {
-    //   console.log("Burger menu is not hidden");
-
-    //   menuBurgerSpace.addEventListener("click", function () {
-    //     console.log("We are clicking the burger menu");
-    //   });
-
-    //   // menuBurgerSpace.addEventListener("mouseleave", function () {
-    //   //   console.log("the mouse left in the burger menu");
-    //   // });
-    // }
+    openBurgerMenu();
   });
 
   // menuBurgerIMG.addEventListener("mouseenter", function () {
   //   console.log("the mouse entered the burger image");
   // });
+  buttonUp.addEventListener("click", function () {
+    menuBurgerSpace.scrollBy({
+      top: -100, // Negative value to scroll up
+      behavior: "smooth", // Optional: smooth scrolling
+    });
+  });
+  buttonDown.addEventListener("click", function () {
+    menuBurgerSpace.scrollBy({
+      top: 100, // Positive value to scroll down
+      behavior: "smooth", // Optional: smooth scrolling
+    });
+  });
 });
