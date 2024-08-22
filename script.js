29f337ad8ce22d9b307658991cac4120bc6ceefe
davidgomezcode <davidgomezcode@gmail.com>
2024-08-22 10:40:33 -0500

    Creation from Js of new activities. In progress: scrollbar to overflow

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 //Selectors:
 const menuBurger = document.querySelector("#menuBurger .hidden");
+const sendButtonParent = document.querySelector(".sendButtonParent");
 
-// Variables
+// Global variables
 let coords;
 let mapEvent;
 //Type of maps tiles (styles):
@@ -32,7 +33,26 @@ let mapReal = L.tileLayer(
 // });
 
 //Functions
-//Function where am I:
+//Function - insertion of new activity:
+const html = `<article class="activity activity-1">
+                <div class="inputParent">
+                  <input class="input activityInput" placeholder="Activity" />
+                </div>
+                <div class="inputParent">
+                  <input class="input timeInput" placeholder="Time" />
+                </div>
+
+                <div class="inputParent">
+                  <textarea
+                    name=""
+                    id=""
+                    class="input observationsInput"
+                    placeholder="Observations"
+                    rows="3"
+                  >
+                  </textarea>
+                </div>
+              </article>`; // variable that contains the structure of the new activity - ON CONSTRUCTION.
 
 //Function of map and geolocation of user:
 navigator.geolocation.getCurrentPosition(function (position) {
@@ -52,10 +72,13 @@ navigator.geolocation.getCurrentPosition(function (position) {
       .openPopup();
   };
 
+  //The coords variable is the current location of the user:
   let { latitude, longitude } = position.coords;
   coords = [latitude, longitude];
+  //Map variable following instructions of Leaflet:
   let map = L.map("map").setView(coords, 16);
 
+  //Choosing the style of the map tile (two options, real, or drawn):
   //   mapReal.addTo(map);
   mapDrawn.addTo(map);
 
@@ -66,7 +89,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
 
   //Event listener to difFerentiate between the clicks over tu whereAmIButtonIMG and over the rest of the map:
   document.addEventListener("click", function (e) {
-    console.log(e.target);
+    // console.log(e.target);
 
     if (e.target.classList.contains("whereAmIButtonIMG")) {
       L.marker(coords).addTo(map);
@@ -75,6 +98,9 @@ navigator.geolocation.getCurrentPosition(function (position) {
       // L.marker(coords, { icon: whereAmIIcon }).addTo(map); //
     } else if (e.target.classList.contains("menuBurgerIMG")) {
       menuBurger.classList.toggle("hidden"); // we toggle the space where the activities are going to be listed
+    } else if (e.target.classList.contains("sendButton")) {
+      console.log("the send button click is heard");
+      sendButtonParent.insertAdjacentHTML("beforebegin", html);
     } else if (
       //if the click is not on
       // the where am I button
@@ -83,6 +109,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
       !e.target.classList.contains("activityInput") && // the input
       !e.target.classList.contains("input") && // the activity
       !e.target.classList.contains("activity") && //the burger menu space
+      !e.target.classList.contains("sendButton") && // It is not the send p¿button
       e.target.id !== "menuBurgerSpace"
     ) {
       // Then, create a marker where the user clicked.
